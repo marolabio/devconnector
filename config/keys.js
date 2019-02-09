af4fbe2bd601f7842c3eaaba6926b5ec61198e4a
Omar Labio <marolabio@gmail.com>
2019-02-09 20:10:04 +0800

    Add JWT auth

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb://devconnector1:devconnector1@ds127825.mlab.com:27825/devconnnector"
+    "mongodb://devconnector1:devconnector1@ds127825.mlab.com:27825/devconnnector",
+  secretOrKey: "secret"
 };
